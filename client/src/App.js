428db8324a54a75c8b356b2f8a84339f1b8e14e4
aneesh072 <aneeshkarki292@gmail.com>
2022-10-08 16:58:09 +1100

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Main from './components/Main';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 const App = () => {
-  return <div>App</div>;
+  const user = localStorage.getItem('token');
+  return (
+    <Routes>
+      {user && <Route path="/" exact element={<Main />} />}
+      <Route path="/signup" exact element={<Signup />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/" exact element={<Navigate replace to="/login" />} />
+    </Routes>
+  );
 };
 
 export default App;
