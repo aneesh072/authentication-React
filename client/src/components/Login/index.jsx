85ea66743f35bd6612edb31f75e2cef6140b1a3b
aneesh072 <aneeshkarki292@gmail.com>
2022-10-08 16:36:27 +1100

    added login component

---
+++ client/src/components/Login/index.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import styles from './styles.module.css';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Signup = () => {
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+  });
+  const [error, setError] = useState('');
+
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.prevendDefault();
+    try {
+      const url = 'http//localhost:8080/api/auth';
+      const { data: res } = await axios.post(url, data);
+      localStorage.setItem('token', res.data);
+      window.location = '/';
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
+  return (
+    <div className={styles.login_container}>
+      <div className={styles.login_form_container}>
+        <div className={styles.left}>
+          <form className={styles.form_container}>
+            <h1>Login to your account</h1>
+
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+              value={data.email}
+              required
+              className={styles.input}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+              value={data.password}
+              required
+              className={styles.input}
+            />
+            {error && <div className={styles.error_msg}>{error}</div>}
+            <button
+              type="submit"
+              className={styles.green_btn}
+              onSubmit={handleSubmit}
+            >
+              Sign In
+            </button>
+          </form>
+        </div>
+        <div className={styles.right}>
+          <h1>New Here? </h1>
+          <Link to="/signpu">
+            <button type="button" className={styles.white_btn}>
+              Sign up
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
