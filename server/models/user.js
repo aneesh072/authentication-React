1cac8afa7744259c3376a47248d5747e8bbea16d
aneesh072 <aneeshkarki292@gmail.com>
2022-10-08 15:18:12 +1100

    added user schema and validation

---
+++ server/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+const passwordComplexity = require('joi-password-complexity');
+const { required } = require('joi');
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, require: true },
+  lastName: { type: String, require: true },
+  email: { type: String, require: true },
+  password: { type: String, require: true },
+});
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
+    expiresIn: '7d',
+  });
+  return token;
+};
+
+const User = mongoose.model('people', userSchema);
+
+const validate = (data) => {
+  const schema = Joi.object({
+    firstName: Joi.string().required().label('First Name'),
+    lastName: Joi.string().required().label('Last Name'),
+    email: Joi.string().email().required().label('Email'),
+    password: passwordComplexity().required().label('Password'),
+  });
+  return schema.validate(data);
+};
+
+module.exports = { User, validate };
