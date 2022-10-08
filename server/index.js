d8df95db998f96f1914bf460e87e97e201da3033
aneesh072 <aneeshkarki292@gmail.com>
2022-10-08 15:56:50 +1100

    import rout

--- server/index.js
+++ server/index.js
@@ -3,12 +3,18 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const connection = require('./db');
+const userRoutes = require('./routes/users');
+const authRoutes = require('./routes/auth');
 
 //database connection
 connection();
 
 app.use(express.json());
 app.use(cors);
+
+//routes
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Running on port ${port}`));
